80884f5e64a4218bf6d3a1c490d0d00a7129f46c
Navid Shaikh <nshaikh@redhat.com>
2018-03-29 12:43:55 +0530

    Fixes invoking systemctl services and setup script

--- setup.sh
+++ setup.sh
@@ -1,10 +1,14 @@
 echo "Run beanstalkd server container.."
-docker run -d --name beanstalkd_server -p 11300:11300 beanstalkd
+docker run -d --name beanstalkd_server -p 11300:11300 beanstalkd:rhel7
 
 echo "Start dispatcher worker"
-cp scripts/dispatcher-worker.service /etc/systemd/dispatcher-worker.service
+cp scripts/dispatcher-worker.service /etc/systemd/system/dispatcher-worker.service
+systemctl daemon-reload
 systemctl restart dispatcher-worker.service
+systemctl status dispatcher-worker.service
 
 echo "Start scan worker"
-cp scripts/scan-worker.service /etc/systemd/scan-worker.service
+cp scripts/scan-worker.service /etc/systemd/system/scan-worker.service
+systemctl daemon-reload
 systemctl restart scan-worker.service
+systemctl status scan-worker.service
