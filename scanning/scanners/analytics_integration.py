b6bdc49b679d3746d517eda29df02e1094a4945d
Navid Shaikh <nshaikh@redhat.com>
2018-04-10 14:55:45 +0530

    Updates analytics scanner invoker class

--- scanning/scanners/analytics_integration.py
+++ scanning/scanners/analytics_integration.py
@@ -2,7 +2,7 @@
 """This class is a wrapper for running analytics scanner and grab results."""
 import os
 
-from container_pipeline.scanners.base import Scanner
+from scanning.scanners.base import Scanner
 
 
 class AnalyticsIntegration(Scanner):
@@ -11,40 +11,33 @@ class AnalyticsIntegration(Scanner):
     def __init__(self):
         """Name it to Misc Package update check."""
         self.scanner_name = "analytics-integration"
-        self.full_scanner_name = "scanner-analytics-integration:rhel7"
         self.scan_types = ["register"]
+        self.result_file = "analytics_scanner_results.json"
 
-    def scan(self, image_under_test, analytics_server):
+    def scan(self, image, analytics_server):
         """Run the scanner."""
         # initializing a blank list that will contain results from all the
         # scan types of this scanner
         logs = []
         super(AnalyticsIntegration, self).__init__(
-            image_under_test=image_under_test,
-            scanner_name=self.scanner_name,
-            full_scanner_name=self.full_scanner_name,
-            to_process_output=False
+            image=image,
+            scanner=self.scanner_name,
+            result_file=self.result_file,
         )
 
-        os.environ["IMAGE_NAME"] = image_under_test
-        if analytics_server:
-            os.environ["SERVER"] = analytics_server
+        os.environ["IMAGE_NAME"] = image
+        os.environ["SERVER"] = analytics_server
 
-        for _ in self.scan_types:
-            scan_cmd = [
-                "atomic",
-                "scan",
-                "--scanner={}".format(self.scanner_name),
-                "--scan_type={}".format(_),
-                "{}".format(image_under_test)
-            ]
+        for st in self.scan_types:
+            scan_results = self.scan(scan_type=st, process_output=False)
 
-            scan_results = super(AnalyticsIntegration, self).scan(scan_cmd)
+            if not scan_results("status", False):
+                continue
 
-            if not scan_results[0]:
-                return False, None
+            logs.append(scan_results["logs"])
 
-            logs.append(scan_results[1])
+        # invoke base class's cleanup utility
+        self.cleanup()
 
         return True, self.process_output(logs)
 
@@ -59,10 +52,11 @@ class AnalyticsIntegration(Scanner):
         as default, scan type
         """
         data = {}
-        data["scanner_name"] = self.scanner_name
+        data["scanner"] = self.scanner_name
+        data["image_under_test"] = self.image
         data["msg"] = ""
         for i in logs:
-            data["msg"] += i["Summary"]
+            data["msg"] += i.get("Summary", "Failed to run the scanner.")
         data["logs"] = logs
 
         return data
