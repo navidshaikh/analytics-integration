16a068eb8b030ab28c51c456dd8e1053dfefc2f5
Bamacharan Kundu <bamachrn@gmail.com>
2018-05-03 11:20:00 +0530

    Added job template rendering and pushing the job content

--- api_poll/poll_server.py
+++ api_poll/poll_server.py
@@ -6,6 +6,9 @@ This module contains the worker that handles the scanning.
 import json
 import logging
 
+import yaml
+from jinja2 import Environment, FileSystemLoader
+
 from base import BaseWorker
 
 
@@ -24,14 +27,29 @@ class PollServer(BaseWorker):
         this calls the ScannerRunner for performing the scan work
         """
         self.job = job
-        image_under_test = job.get("image_under_test")
-        analytics_server = job.get("analytics_server")
-        git-sha = job.get("git-sha")
-        git-url = job.get("git-url")
+        project["image_under_test"] = job.get("image_under_test")
+        project["analytics_server"] = job.get("analytics_server")
+        project["git-sha"] = job.get("git-sha")
+        project["git-url"] = job.get("git-url")
 
+        try:
+            env = Environment(loader=FileSystemLoader(
+                './'), trim_blocks=True, lstrip_blocks=True)
+            template = env.get_template("api-poll-server.yml")
+            job_details = template.render(project)
+        except Exception as e:
+            print("Error template is not updated: %s" % str(e))
+
+        try:
+             generated_filename = "poll_server_generated.yaml"
+             with open(generated_filename, 'w') as outfile:
+                 outfile.write(job_details)
+                 outfile.flush()
+        except Exception as e:
+            print("Error job_details could not be updated %s" % str(e))
 
 if __name__ == '__main__':
 
     log.load_logger()
     logger = logging.getLogger("poll-server")
-    ScanWorker(logger, sub='poll_server', pub='failed_polling').run()
+    PollServer(logger, sub='poll_server', pub='failed_polling').run()
