37c072d7a187aeda0013a394ea5a24d0d9791e6f
Navid Shaikh <nshaikh@redhat.com>
2018-05-11 13:07:50 +0530

    Improves saasherder parser to not miss repos

--- saasherder_parser/get_repo_details_from_image.sh
+++ saasherder_parser/get_repo_details_from_image.sh
@@ -12,15 +12,28 @@ IMAGE=$1
 IMAGE=$(echo $IMAGE|sed -e "s/\/osio-prod\//\//g")
 NAME_SPACE=$(echo $IMAGE|cut -f2 -d "/")
 APP_NAME=$(echo $IMAGE|cut -f3 -d "/"|cut -f1 -d ":")
-TAG=$(echo $IMAGE|cut -f3 -d "/"|cut -f2 -d ":")
-REPO_DIR=$(grep $NAME_SPACE * -R|grep image|head -1|cut -f1 -d "/")
-cd $REPO_DIR
-CURRENT_CONTEXT=$(grep current config.yaml|cut -f2 -d " ")
-GIT_URL=$(saasherder --context $CURRENT_CONTEXT get url $APP_NAME)
-GIT_HASH=$(saasherder --context $CURRENT_CONTEXT get hash $APP_NAME)
-TAG_LENGTH=$(saasherder --context $CURRENT_CONTEXT get hash_length $APP_NAME)
-IMAGE_TAG=$(echo $GIT_HASH|cut -c1-$TAG_LENGTH)
 
-echo "git-url=$GIT_URL"
-echo "git-sha=$GIT_HASH"
-echo "image-tag=$IMAGE_TAG"
+for item in $(cat repo_list)
+do
+  cd $item
+  CURRENT_CONTEXT=$(grep current config.yaml|cut -f2 -d " ")
+  GIT_URL=$(saasherder --context $CURRENT_CONTEXT get url $APP_NAME)
+  rc=$?
+  if [ $? == 0 ]
+  then
+    GIT_HASH=$(saasherder --context $CURRENT_CONTEXT get hash $APP_NAME)
+    TAG_LENGTH=$(saasherder --context $CURRENT_CONTEXT get has_length $APP_NAME)
+    IMAGE_TAG=$(echo $GIT_HASH|cut -c1-$TAG_LENGTH)
+    echo "git-url=$GIT_URL"
+    echo "git-sha=$GIT_HASH"
+    echo "image-tag=$IMAGE_TAG"
+    cd ../
+    exit 0
+  else
+    echo "GIT_URL not found for $APP_NAME in $CURRENT_CONTEXT, checking next!"
+    cd ../
+    continue
+done
+
+echo "$NAME_SPACE/$APP_NAME not found via saasherder."
+exit 1
