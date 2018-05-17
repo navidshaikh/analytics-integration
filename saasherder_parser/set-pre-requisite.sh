a72f82cc33efe91533c16a5e2bc765a0d19d9974
Navid Shaikh <nshaikh@redhat.com>
2018-05-17 14:43:39 +0530

    Updates the commands in set-pre-requisite.sh

--- saasherder_parser/set-pre-requisite.sh
+++ saasherder_parser/set-pre-requisite.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-yum install -y python-pip || exit 1
-pip install --upgrade pip || exit 1
+yum install -y python-pip
+pip install --upgrade pip
 git clone https://github.com/openshiftio/saasherder
-cd saasherder
-pip install -v -r requirements.txt ||cd ../ && exit 1
-python setup.py install || cd ../ && exit 1
+cd saasherder && pip install -v -r requirements.txt; python setup.py install
 cd ../
 git clone https://github.com/openshiftio/saas-openshiftio
 git clone https://github.com/openshiftio/saas-analytics
