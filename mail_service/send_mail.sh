d3a7166f38110744f990a3c4ade7475f5a2586bd
Navid Shaikh <nshaikh@redhat.com>
2018-05-18 19:55:14 +0530

    Sends a single email with optional email ids

--- mail_service/send_mail.sh
+++ mail_service/send_mail.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-echo -e $3 |mail -r nshaikh@redhat.com -S smtp=smtp://smtp.corp.redhat.com -s "$1" $4 $2
-
-echo -e $3 |mail -r nshaikh@redhat.com -S smtp=smtp://smtp.corp.redhat.com -s "$1" $4 nshaikh@redhat.com bkundu@redhat.com aagshah@redhat.com schoudhu@redhat.com
+echo -e $3 |mail -r nshaikh@redhat.com -c nshaikh@redhat.com -c bkundu@redhat.com -c aagshah@redhat.com -c schoudhu@redhat.com -S smtp=smtp://smtp.corp.redhat.com -s "$1" $4 $2
