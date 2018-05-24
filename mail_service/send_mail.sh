3081597ea7d48e5bc1441d3a83d1c07904dd5441
Navid Shaikh <nshaikh@redhat.com>
2018-05-24 17:21:05 +0530

    Adds ML email in from and Cc address of mail command

--- mail_service/send_mail.sh
+++ mail_service/send_mail.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo -e $3 |mail -r nshaikh@redhat.com -c nshaikh@redhat.com -c bkundu@redhat.com -c aagshah@redhat.com -c schoudhu@redhat.com -S smtp=smtp://smtp.corp.redhat.com -s "$1" $4 $2
+echo -e $3 |mail -r images-scan@redhat.com -c images-scan@redhat.com -S smtp=smtp://smtp.corp.redhat.com -s "$1" $4 $2
