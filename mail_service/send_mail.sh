260ebcac002282ca6871a10c200f74fdf7676deb
Navid Shaikh <nshaikh@redhat.com>
2018-06-05 16:48:57 +0530

    Removes mail_services module

--- mail_service/send_mail.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-echo -e $3 |mail -r images-scan@redhat.com -c images-scan@redhat.com -S smtp=smtp://smtp.corp.redhat.com -s "$1" $4 $2
--- mail_service/start_mail_server.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export REPLYTO=nshaikh@redhat.com
-mkfifo /var/spool/postfix/public/pickup
-postfix start
-sleep 20
-python /root/analytics-integration/mail_service/worker_notify_user.py
