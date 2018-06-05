69013c26b04147c305f2553f3658de2c54ace79d
Navid Shaikh <nshaikh@redhat.com>
2018-06-05 15:49:05 +0530

    Adds configurable option for git repository to store scan alerts
     Also removes email address

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
 ANALYTICS_SERVER = ""
-NOTIFY_EMAILS = ["nshaikh@redhat.com"]
 
-# ===============email-configs===================
+# ===============alert-configs===================
 #  possible types ["ok", "problem"]
 ALERTS = ["problem"]
+
+# enter path to configured git repo with write and push access
+# alerts will be added in this repo and pushed upon scan completion
+GITREPO=
