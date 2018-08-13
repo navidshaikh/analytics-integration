899fe7d490e49ec75f3adbf4ba07743a4944d5f2
Navid Shaikh <nshaikh@redhat.com>
2018-08-13 09:25:03 +0530

    Push scan results to git irrespective of the alerts config
     desired new behavior: Irrespective of the results of scanner, as in
     whether the results need to be alerted, log every container scanned
     and its results to configured git repository.
    
     Updated the default config to alert for "ok" as well as "problem"
     identified for scanned containers.

--- config.py
+++ config.py
@@ -2,7 +2,11 @@ ANALYTICS_SERVER = ""
 
 # ===============alert-configs===================
 #  possible types ["ok", "problem"]
-ALERTS = ["problem"]
+# if specified only "problem" like ["problem"],
+# only containers with problems will be reported
+# if specified both like ["ok", "problem"],
+# every container scanned will be reported
+ALERTS = ["ok", "problem"]
 
 # enter path to configured git repo with write and push access
 # alerts will be added in this repo and pushed upon scan completion
